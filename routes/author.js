a1425102ab22239d5a7adf83a3ba8ecf7b8c5a9b
Camila <contactcamilasanchez@gmail.com>
2024-07-05 21:23:38 -0300

    settings page basis

--- routes/author.js
+++ routes/author.js
@@ -25,6 +25,7 @@ router.get('/:id', async (req, res, next) => {
 
     variables = {
       authorName: author.user_name,
+      authorId: req.params.id,
       blogTitle: author.blog_title
         ? author.blogTitle
         : `The blog of ${author.user_name}`,
@@ -43,6 +44,31 @@ router.get('/:id', async (req, res, next) => {
 
     // Render the page with the results
     res.render('author/home.ejs', variables);
+  } catch (err) {
+    next(err); //send the error on to the error handler
+  }
+});
+
+router.get('/:id/settings', async (req, res, next) => {
+  let variables = {};
+
+  try {
+    // Define the query to users table
+    const userQuery = `SELECT user_name, blog_title FROM users WHERE user_id=${req.params.id}`;
+    // Execute the query
+    const author = await dbGet(userQuery);
+
+    if (!author) {
+      return res.status(404).send('User not found');
+    }
+
+    variables = {
+      authorName: author.user_name,
+      authorId: req.params.id,
+      blogTitle: author.blog_title,
+    };
+
+    res.render('author/settings.ejs', variables);
   } catch (err) {
     next(err); //send the error on to the error handler
   }
