5f0e79bb503743d879b6101dfbcb67750b6bdb5b
Camila <contactcamilasanchez@gmail.com>
2024-07-13 13:31:04 -0300

    improve read helper doc

--- helpers/reads.js
+++ helpers/reads.js
@@ -1,17 +1,27 @@
 const { dbRun } = require('./promises.js');
 
-/** Update read count by 1 for the selected articleId */
-const addRead = async articleId => {
+/**
+ * @desc Increaes read count by 1 for the selected article
+ * @param {number} articleId - The id of the article to increase the read count
+ * @returns {Promise}
+ */
+const addRead = articleId => {
   const addRead =
     'UPDATE articles SET number_of_reads = number_of_reads + 1 WHERE article_id = ?';
-  await dbRun(addRead, [articleId]);
+
+  return dbRun(addRead, [articleId]);
 };
 
-/** Decrease read count by 1 for the selected articleId */
+/**
+ * @desc Decreases read count by 1 for the selected article
+ * @param {number} articleId - The id of the article to decrease the read count
+ * @returns {Promise}
+ */
 const removeRead = async articleId => {
   const addRead =
     'UPDATE articles SET number_of_reads = number_of_reads - 1 WHERE article_id = ?';
-  await dbRun(addRead, [articleId]);
+
+  return dbRun(addRead, [articleId]);
 };
 
 module.exports = {
